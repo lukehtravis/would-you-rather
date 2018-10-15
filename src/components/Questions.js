8d76a20fa5a83e95744f17dec9c2f8d11b6bacc7
luke <lukehtravis@gmail.com>
2018-10-14 20:24:16 -0700

    feat: added ability of questions page to receive and display data from store

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -4,15 +4,56 @@ import ListedQuestion from './ListedQuestion'
 
 class Questions extends Component {
   render() {
-    let answeredQuestions = []
-    let unansweredQuestions = []
+    let answered = []
+    let unanswered = []
+    // Used to sort questions into answered vs unanswered sections for rendering
+    this.props.questionsKeys.map(question => {
+      if (this.props.questionsAnswered.includes(this.props.questions[question].id) == true) {
+        answered.push(this.props.questions[question])
+      } else {
+        unanswered.push(this.props.questions[question])
+      }
+    })
     return (
-      <div className="questions">
-        <div className="unanswered-questions-list">
-
+      <div className="questions-page">
+        <div className="questions-list">
+          <div className="questions-list-question">
+            <p></p>
+          </div>
         </div>
-        <div className="answered-questions-list">
-
+        <div className="questions-list">
+          {answered.map(question => (
+            <div className="questions-list-question">
+              <div className="questions-list-header">{question.author}</div>
+              <div className="questions-list-body">
+                <div>
+                  <img />
+                </div>
+                <div className="questions-list-question-text">
+                  <p>Would You Rather</p>
+                  <p>{question.optionOne.text}</p>
+                  <a>Link To Individual Poll</a>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="questions-list">
+          {unanswered.map(question => (
+            <div className="questions-list-question">
+              <div className="questions-list-header">{question.author}</div>
+              <div className="questions-list-body">
+                <div>
+                  <img />
+                </div>
+                <div className="questions-list-question-text">
+                  <p>Would You Rather</p>
+                  <p>{question.optionOne.text}</p>
+                  <a>Link To Individual Poll</a>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
 
@@ -22,12 +63,14 @@ class Questions extends Component {
 
 function mapStateToProps({questions, authenticatedUser, users}) {
   let questionIds = Object.keys(questions)
+  let answers = []
+  // If statement necessart because mapStateToProps is run for each action
   if (users[authenticatedUser] != undefined) {
-    console.log(users[authenticatedUser].answers)
+    answers = Object.keys(users[authenticatedUser].answers)
   }
   return {
     questions: questions,
-    questionsAnswered: users[authenticatedUser],
+    questionsAnswered: answers,
     questionsKeys: questionIds,
     authedUser: authenticatedUser
   }

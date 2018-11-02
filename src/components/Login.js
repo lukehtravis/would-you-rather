6f5f1d7914e9fb4a961d52207d04787dd8c5f06a
luke <lukehtravis@gmail.com>
2018-11-02 13:48:25 -0700

    feat: add setAuthenticated user action to login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,17 +1,23 @@
 import React, {Component} from "react"
 import {connect} from "react-redux"
+import {setAuthenticatedUser} from '../actions/authenticatedUser'
 
 class Login extends Component {
+  handleSubmit() {
+    var e = document.getElementById("user-dropdown");
+    var user = e.options[e.selectedIndex].value;
+    this.props.dispatch(setAuthenticatedUser(user))
+  }
   render() {
     return (
       <div className="login-page">
         <div>Please Choose A User to Login As</div>
-        <select>
+        <select id="user-dropdown">
           <option value="sarahedo">Sarah Edo</option>
           <option value="tylermcginnis">Tyler McGinnis</option>
           <option value="johndoe">John Doe</option>
         </select>
-        <button>Submit</button>
+        <button onClick={() => this.handleSubmit()}>Submit</button>
       </div>
     )
   }

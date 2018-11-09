c173d820b035adffa25ac382b91526eac055f177
luke <lukehtravis@gmail.com>
2018-11-09 08:24:17 -0800

    feat: switching between answered and unanswered questions on homepage

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -3,6 +3,14 @@ import {connect} from 'react-redux'
 import { BrowserRouter as Router, Route, Link, withRouter } from 'react-router-dom'
 
 class Questions extends Component {
+  state = {
+    showing: "unanswered"
+  }
+  handleClick = (e) => {
+    this.setState(({
+      showing: e.target.className
+    }))
+  }
   render() {
     let answered = []
     let unanswered = []
@@ -15,44 +23,49 @@ class Questions extends Component {
       }
     })
     return (
-
       <div className="questions-page">
-        <div className="questions-list">
-          {answered.map(question => (
-            <div key={question.id} className="questions-list-question">
-              <div className="questions-list-header">{question.author}</div>
-              <div className="questions-list-body">
-                <div>
-                  <img src={this.props.users[question.author].avatarURL} />
-                </div>
-                <div className="questions-list-question-text">
-                  <p>Would You Rather</p>
-                  <p>{question.optionOne.text}</p>
-                  <Link to={`/questions/${question.id}`}>Link To Individual Poll</Link>
+        <div className="questions-toggle">
+          <button className="unanswered" onClick={(e) => this.handleClick(e)}>Unansered</button>
+          <button className="answered" onClick={(e) => this.handleClick(e)}>Answered</button>
+        </div>
+        {this.state.showing == "unanswered" ? (
+          <div className="unanswered">
+            {unanswered.map(question => (
+              <div key={question.id} className="questions-list-question">
+                <div className="questions-list-header">{question.author}</div>
+                <div className="questions-list-body">
+                  <div>
+                    <img  />
+                  </div>
+                  <div className="questions-list-question-text">
+                    <p>Would You Rather</p>
+                    <p>{question.optionOne.text}</p>
+                    <Link to={`/questions/${question.id}`}>Link To Individual Poll</Link>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
-        </div>
-        <div className="questions-list">
-          {unanswered.map(question => (
-            <div key={question.id} className="questions-list-question">
-              <div className="questions-list-header">{question.author}</div>
-              <div className="questions-list-body">
-                <div>
-                  <img  />
-                </div>
-                <div className="questions-list-question-text">
-                  <p>Would You Rather</p>
-                  <p>{question.optionOne.text}</p>
-                  <Link to={`/questions/${question.id}`}>Link To Individual Poll</Link>
+            ))}
+          </div>
+        ) : (
+          <div className="answered">
+            {answered.map(question => (
+              <div key={question.id} className="questions-list-question">
+                <div className="questions-list-header">{question.author}</div>
+                <div className="questions-list-body">
+                  <div>
+                    <img src={this.props.users[question.author].avatarURL} />
+                  </div>
+                  <div className="questions-list-question-text">
+                    <p>Would You Rather</p>
+                    <p>{question.optionOne.text}</p>
+                    <Link to={`/questions/${question.id}`}>Link To Individual Poll</Link>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
+        )}
       </div>
-
     )
   }
 }

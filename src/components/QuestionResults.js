a404b5a730e46058d967dcbc3afc5279c11531a7
luke <lukehtravis@gmail.com>
2018-11-24 12:50:42 -0800

    feature: highlight answer of current user in question results component

--- src/components/QuestionResults.js
+++ src/components/QuestionResults.js
@@ -3,10 +3,10 @@ import {connect} from "react-redux"
 import {withRouter} from "react-router-dom"
 
 class QuestionResults extends Component {
-
   render() {
-    let {question, properName, imgUrl} = this.props
+    let {question, properName, imgUrl, answerChosen} = this.props
     let total = question.optionOne.votes.length + question.optionTwo.votes.length
+
     return (
       <div className="question-view">
         <div className="question-view-header">
@@ -17,16 +17,16 @@ class QuestionResults extends Component {
             <img alt="user avatar" src={imgUrl}/>
           </div>
           <div className="question-view-result">
-            <h2>Results</h2>
-            <div className="question-view-answer">
-              <p className="question-view-text"></p>
+            <h2>Results (Your response highlighted in green)</h2>
+            <div id={this.props.answerChosen === "optionOne" ? "selected" : "not-selected"} className="question-view-answer">
+              <p className="question-view-text">{question.optionOne.text}</p>
               <div className="question-view-percent">
                 {Math.floor((question.optionOne.votes.length / total) * 100)}%
               </div>
               <p className="question-view-count">{question.optionOne.votes.length} out of {total} votes</p>
             </div>
-            <div className="question-view-answer">
-              <p className="question-view-text"></p>
+            <div id={this.props.answerChosen === "optionTwo" ? "selected" : "not-selected"} className="question-view-answer">
+              <p className="question-view-text">{question.optionTwo.text}</p>
               <div className="question-view-percent">
                 {Math.floor((question.optionTwo.votes.length / total) * 100)}%
               </div>
@@ -42,11 +42,13 @@ class QuestionResults extends Component {
 function mapStateToProps({authenticatedUser, questions, users}, props) {
   const {id} = props.match.params
   let viewedQuestion = questions[id]
+  let answerToCurrentQuestion = users[authenticatedUser].answers[viewedQuestion.id]
   return {
     question: viewedQuestion,
     authedUser: authenticatedUser,
     properName: users[viewedQuestion.author].name,
-    imgUrl: users[viewedQuestion.author].avatarURL
+    imgUrl: users[viewedQuestion.author].avatarURL,
+    answerChosen: answerToCurrentQuestion
   }
 }
 

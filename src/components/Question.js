94cf84aeff645bada40d3be27845433bf427d09c
luke <lukehtravis@gmail.com>
2018-11-24 17:04:04 -0800

    bug: ensure user has selected option before submitting answer to question

--- src/components/Question.js
+++ src/components/Question.js
@@ -5,12 +5,22 @@ import {handleAnswerQuestion} from '../actions/users'
 import {handleAnswerQuestionQ} from '../actions/questions'
 import QuestionResults from '../components/QuestionResults'
 
-
-
 class Question extends Component {
+  state = {
+    radioValue: undefined
+  }
+  radioClick(e) {
+    this.setState({
+      radioValue: e.target.value
+    })
+  }
   handleSubmit() {
-    this.props.dispatch(handleAnswerQuestion(this.props.individualQuestion.id, document.querySelector('input[name="options"]:checked').value, this.props.authedUser))
-    this.props.dispatch(handleAnswerQuestionQ(this.props.individualQuestion.id, document.querySelector('input[name="options"]:checked').value, this.props.authedUser))
+    if (this.state.radioValue !== undefined) {
+      this.props.dispatch(handleAnswerQuestion(this.props.individualQuestion.id, document.querySelector('input[name="options"]:checked').value, this.props.authedUser))
+      this.props.dispatch(handleAnswerQuestionQ(this.props.individualQuestion.id, document.querySelector('input[name="options"]:checked').value, this.props.authedUser))
+    } else {
+      alert("please select an option and re-submit")
+    }
   }
   render() {
     if (!this.props.individualQuestion) {
@@ -26,9 +36,9 @@ class Question extends Component {
           <div className="questions-create-body">
             <div className="questions-list-question-text">
               <p>Would you rather</p>
-              <p><input type="radio" name="options" value="optionOne" />{this.props.individualQuestion.optionOne.text}</p>
+              <p><input type="radio" name="options" value="optionOne" onClick={(e) => this.radioClick(e)} />{this.props.individualQuestion.optionOne.text}</p>
               <p>Or</p>
-              <p><input type="radio" name="options" value="optionTwo" />{this.props.individualQuestion.optionTwo.text}</p>
+              <p><input type="radio" name="options" value="optionTwo" onClick={(e) => this.radioClick(e)} />{this.props.individualQuestion.optionTwo.text}</p>
               <button onClick={() => this.handleSubmit() }>Submit</button>
             </div>
           </div>
